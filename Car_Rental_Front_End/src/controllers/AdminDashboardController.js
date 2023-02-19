14c68f49866c1423e0c19e5558b8646bd5064fee
VihangaIndeewra <indeewravihnga@gmail.com>
2023-02-19 09:53:21 +0530

    Added Customer Manager Form

--- Car_Rental_Front_End/src/controllers/AdminDashboardController.js
+++ Car_Rental_Front_End/src/controllers/AdminDashboardController.js
@@ -2,6 +2,7 @@ $("#home").click(function (){
     $("#HomePage").css("display","block")
     $("#CarManagerPage").css("display","none")
     $("#AdminPage").css("display","none")
+    $("#CustomersManagerPage").css("display","none")
 
 });
 
@@ -9,10 +10,19 @@ $("#cars").click(function (){
     $("#HomePage").css("display","none")
     $("#CarManagerPage").css("display","block")
     $("#AdminPage").css("display","none")
+    $("#CustomersManagerPage").css("display","none")
 });
 
 $("#admin").click(function (){
     $("#HomePage").css("display","none")
     $("#CarManagerPage").css("display","none")
     $("#AdminPage").css("display","block")
+    $("#CustomersManagerPage").css("display","none")
+})
+
+$("#customers").click(function (){
+    $("#HomePage").css("display","none")
+    $("#CarManagerPage").css("display","none")
+    $("#AdminPage").css("display","none")
+    $("#CustomersManagerPage").css("display","block")
 })
