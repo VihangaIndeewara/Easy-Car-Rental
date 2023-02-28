9b39822291fd3dd8e5fc5c8b7af3aac8392ff4dd
VihangaIndeewra <indeewravihnga@gmail.com>
2023-02-28 19:26:17 +0530

    Developed Vehicle Booking Form

--- Car_Rental_Front_End/src/controllers/CustomerDashboardController.js
+++ Car_Rental_Front_End/src/controllers/CustomerDashboardController.js
@@ -1,4 +1,11 @@
 $("#btnSearchVehicle").click(function (){
     $("#CustomerDashboardPage").css("display","none")
     $("#VehiclePage").css("display","block")
+    $("#VehicleBookingPage").css("display","none")
+})
+
+$(".bookNow").click(function (){
+    $("#CustomerDashboardPage").css("display","none")
+    $("#VehiclePage").css("display","none")
+    $("#VehicleBookingPage").css("display","block")
 })
