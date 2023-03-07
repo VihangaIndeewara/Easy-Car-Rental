68082aa2bad95e4a768c73df344bbf364c4b2499
VihangaIndeewra <indeewravihnga@gmail.com>
2023-03-07 15:48:42 +0530

    Added Save Car

---
+++ Car_Rental_Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/car")
+@CrossOrigin
+public class CarController {
+
+    @Autowired
+    CarService service;
+
+    @PostMapping
+    public ResponseUtil saveCar(CarDTO dto){
+        service.addCar(dto);
+        return new ResponseUtil("200","Successfully Added",null);
+    }
+}
--- Car_Rental_Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
+++ Car_Rental_Back_End/src/main/java/lk/ijse/spring/service/impl/CarServiceImpl.java
@@ -1,11 +1,30 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.entity.Car;
+import lk.ijse.spring.repo.CarRepo;
 import lk.ijse.spring.service.CarService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class CarServiceImpl implements CarService {
+    @Autowired
+    CarRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
+
     @Override
     public void addCar(CarDTO dto) {
-
+        if (repo.existsById(dto.getCId())){
+            throw new RuntimeException("Admin Id "+dto.getCId()+" Already Exit..");
+        }
+        Car entity = mapper.map(dto, Car.class);
+        repo.save(entity);
     }
 }
